b22b87b3857e2324eca486368385f93b7988c078
alphakenshin9@gmail.com <69D7YQavm^m2QU_>
2021-02-20 04:14:23 +0200

    Удаление метода protected function outputData() из шаблонизатора фреймворка.

--- core/user/controllers/IndexController.php
+++ core/user/controllers/IndexController.php
@@ -7,20 +7,20 @@ use core\base\controllers\BaseController;
 class IndexController extends BaseController {
 
     protected  $name;
-    // trait - подключается через конструкцию use, но вне тела метода.
-    use trait1, trait2 {
-        //Методом who первого трейта заменить метод who второго трейта.
-        trait1::who insteadof trait2;
-        // А если надо использовать одноименнный метод и однго трейта и другого, то...
-
-        trait2::who as who2;
-        //Но, псевдоним можно объявить только для такого метода, который уже был замещен иным методом
-    }
+//    // trait - подключается через конструкцию use, но вне тела метода.
+//    use trait1, trait2 {
+//        //Методом who первого трейта заменить метод who второго трейта.
+//        trait1::who insteadof trait2;
+//        // А если надо использовать одноименнный метод и однго трейта и другого, то...
+//
+//        trait2::who as who2;
+//        //Но, псевдоним можно объявить только для такого метода, который уже был замещен иным методом
+//    }
 
     protected function inputData() {
         //Трейт - это механизм обеспечения повторного использования кода в языках с поддержкой только одиночного наследования,
-        $this->who();
-        $this->who2();
+//        $this->who();
+//        $this->who2();
 
 
         exit();
@@ -38,11 +38,11 @@ class IndexController extends BaseController {
 //        echo '<h1 style="color: red">'; $name;
     }
 
-    protected function outputData() {
-        // В $vars - принимаем переменные $content, $header, $footer
-        $vars = func_get_arg(0);
-//
-//        exit($this->render('', $vars));
-        $this->page = $this->render(TEMPLATE . 'templater', $vars);
-    }
+//    protected function outputData() {
+//        // В $vars - принимаем переменные $content, $header, $footer
+//        $vars = func_get_arg(0);
+////
+////        exit($this->render('', $vars));
+//        $this->page = $this->render(TEMPLATE . 'templater', $vars);
+//    }
 }
